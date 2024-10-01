4a0e8e7a607d9e0258f572b2aa588be34e565302
Nicolas77777777 <140002852+Nicolas77777777@users.noreply.github.com>
2024-10-01 21:56:12 +0200

    first commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class  Main{
+
+    public static void main(String[] args) {
+        int x;
+        x= 45;
+
+        int y;
+        y= 34;
+
+        int franco;
+        franco= 1 ;
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(franco);
+        
+
+    }
+}
